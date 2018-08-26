c2d35bb9a6e919fb68ce4e629486b77f0fd47da8
cde <cdechauri@gmail.com>
2018-08-25 18:46:19 -0700

    Setup DB

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+-- create DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  burger_name varchar(125) NOT NULL,
+  devoured BOOLEAN DEFAULT false
+)
